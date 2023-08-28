0474430885a866783c9d273132e9c81a7fff3ba0
AlexAbrello <abrello063@gmail.com>
2023-08-28 13:47:15 +0300

    feat: logim form

--- src/components/auth/login-form/login-form.tsx
+++ src/components/auth/login-form/login-form.tsx
@@ -1,15 +1,15 @@
-import { Button } from '@/components/ui'
-import { TextField } from '@/components/ui'
+import { Button, CheckboxComponent, TextField } from '@/components/ui'
 import { useForm } from 'react-hook-form'
 
 type FormValues = {
    email: string
    password: string
+   rememberMe: boolean
 }
 
 export const LoginForm = () => {
 
-   const {register, handleSubmit} = useForm<FormValues>()
+   const { register, handleSubmit } = useForm<FormValues>()
 
    const onSubmit = (data: FormValues) => {
       console.log(data)
@@ -18,7 +18,8 @@ export const LoginForm = () => {
   return (
    <form onSubmit={handleSubmit(onSubmit)}>
       <TextField placeholder={'введите e-mail'} {...register('email')} label={'email'} />
-      <TextField placeholder={'введите пароль'} {...register('password')} label={'password'} />
+      <TextField placeholder={'введите пароль'} {...register('password')} label={'password'} type='password'/>
+      <CheckboxComponent {...register('rememberMe')} label={'Remember Me'} />
       <Button type="submit">Submit</Button>
     </form>
   )
