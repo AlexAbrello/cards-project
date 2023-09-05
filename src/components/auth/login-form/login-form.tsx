90e1729c889909c48e72dd9eea38421ea293f5a8
AlexAbrello <abrello063@gmail.com>
2023-09-05 11:55:31 +0300

    feat: add card.tsx

--- src/components/auth/login-form/login-form.tsx
+++ src/components/auth/login-form/login-form.tsx
@@ -2,9 +2,9 @@ import { DevTool } from '@hookform/devtools'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { useForm } from 'react-hook-form'
 
-import { Button, ControlledCheckbox, ControlledTextField } from '@/components/ui'
+import { Button, Card, ControlledCheckbox, ControlledTextField } from '@/components/ui'
+import { Typography } from '@/components/ui/typography'
 import { FormValues, loginSchema } from '@/types/login-form/login-shema.ts'
-
 export const LoginForm = () => {
   const {
     control,
@@ -19,26 +19,31 @@ export const LoginForm = () => {
   }
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <DevTool control={control} />
-      <ControlledTextField
-        name={'email'}
-        control={control}
-        errorMessage={errors?.email?.message}
-        type="text"
-        label={'email'}
-        placeholder={'Enter e-mail'}
-      />
-      <ControlledTextField
-        name={'password'}
-        control={control}
-        errorMessage={errors?.password?.message}
-        type="password"
-        label={'password'}
-        placeholder={'Enter password'}
-      />
-      <ControlledCheckbox name={'rememberMe'} control={control} label={'Remember Me'} />
-      <Button type="submit">Submit</Button>
-    </form>
+    <Card>
+      <Typography.H1>Sign In</Typography.H1>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <DevTool control={control} />
+        <ControlledTextField
+          name={'email'}
+          control={control}
+          errorMessage={errors?.email?.message}
+          type="text"
+          label={'Email'}
+          placeholder={'Enter e-mail'}
+        />
+        <ControlledTextField
+          name={'password'}
+          control={control}
+          errorMessage={errors?.password?.message}
+          type="password"
+          label={'Password'}
+          placeholder={'Enter password'}
+        />
+        <ControlledCheckbox name={'rememberMe'} control={control} label={'Remember Me'} />
+        <Button type="submit" fullWidth={true}>
+          <Typography.Subtitle2>Submit</Typography.Subtitle2>
+        </Button>
+      </form>
+    </Card>
   )
 }
