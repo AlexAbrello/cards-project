9f50bf6c7553401e53724f428132b166e8d4fd9d
AlexAbrello <abrello063@gmail.com>
2023-09-14 09:21:48 +0300

    feat: routes

--- src/components/auth/check-email-form/check-email-form.tsx
+++ src/components/auth/check-email-form/check-email-form.tsx
@@ -1,14 +1,20 @@
+import { FC } from 'react'
+
 import EmailImage from '@/assets/icons/email-image.tsx'
 import { Button, Card } from '@/components/ui'
 import { Typography } from '@/components/ui/typography'
 
-export const CheckEmail = () => {
+type CheckEmailProps = {
+  email: string
+}
+
+export const CheckEmail: FC<CheckEmailProps> = ({ email = 'example@mail.com' }) => {
   return (
     <Card>
       <Typography.H1>Check Email</Typography.H1>
       <EmailImage />
       <Typography.Body2 style={{ color: '#4C4C4C', textAlign: 'center' }}>
-        We’ve sent an Email with instructions to example@mail.com
+        We’ve sent an Email with instructions to {email}
       </Typography.Body2>
       <Button as={'a'} href="#" fullWidth={true}>
         <Typography.Subtitle2>Back to Sign In</Typography.Subtitle2>
