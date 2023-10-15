5903095cff8ef86282b2b97d45c771489cf57207
AlexAbrello <abrello063@gmail.com>
2023-10-15 16:17:50 +0300

    feat: add create-card.tsx

---
+++ src/pages/decks/empty-deck/empty-deck.tsx
@@ -0,0 +1,9 @@
+import { Typography } from '@/components/ui/typography'
+
+export const EmptyDeck = () => {
+  return (
+    <div>
+      <Typography.Body1>This pack is empty. Click add new card to fill this pack</Typography.Body1>
+    </div>
+  )
+}
